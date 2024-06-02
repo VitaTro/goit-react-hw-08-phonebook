4d28d8cf7539bae7328509c70a960da1f2d1a261
VitaTro <144832571+VitaTro@users.noreply.github.com>
2024-06-02 11:13:33 +0200

    020620244

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,5 +1,6 @@
 import { useDispatch } from 'react-redux';
 import { login } from '../../redux/auth/operations';
+import { Button, Form, Input, Label, Section } from './LoginForm.styled';
 
 // відповідає за форму авторизації користувача
 export const LoginForm = () => {
@@ -20,30 +21,32 @@ export const LoginForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Email
-        <input
-          type="email"
-          name="email"
-          placeholder="email"
-          pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-          title="Please enter a valid email address"
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          name="password"
-          placeholder="password"
-          pattern="^[a-zA-Z0-9!@#$%^&*()-_=+`~[\]{}|:<>/?]+$"
-          title="The password should contain only Latin letters (both uppercase and lowercase), numbers and other symbols"
-          required
-        />
-      </label>
-      <button type="submit"> Log In</button>
-    </form>
+    <Section>
+      <Form onSubmit={handleSubmit}>
+        <Label>
+          Email
+          <Input
+            type="email"
+            name="email"
+            placeholder="email"
+            pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+            title="Please enter a valid email address"
+            required
+          />
+        </Label>
+        <Label>
+          Password
+          <Input
+            type="password"
+            name="password"
+            placeholder="password"
+            pattern="^[a-zA-Z0-9!@#$%^&*()-_=+`~[\]{}|:<>/?]+$"
+            title="The password should contain only Latin letters (both uppercase and lowercase), numbers and other symbols"
+            required
+          />
+        </Label>
+        <Button type="submit"> Log In</Button>
+      </Form>
+    </Section>
   );
 };
---
+++ src/components/RegisterForm/RegisterForm.styled.jsx
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+export const Section = styled.section`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: radial-gradient(circle at center, #ffeb3b 5%, #f89498 45%);
+  z-index: -1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  padding: 21px;
+  border: 3px solid brown;
+  border-radius: 15px;
+  height: 50%;
+  margin: 0 auto;
+`;
+
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+  color: #000000;
+  font-size: 20px;
+`;
+
+export const Input = styled.input`
+  color: #53545af5;
+  padding: 8px 12px;
+  font: inherit;
+  cursor: pointer;
+  margin-top: 5px;
+`;
+
+export const Button = styled.button`
+  padding: 8px 10px;
+  font: inherit;
+  cursor: pointer;
+  border-radius: 4px;
+  border: 3px solid #3c8092;
+  color: #fff;
+  background-color: #4cb0d4;
+  font-size: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    background-color: #4cd8d6;
+  }
+`;
